fef30e08f9b0c3a4d28883ba876197e49841a970
Nyzio <norja229@gmail.com>
2023-07-02 23:09:52 +0200

    register error fixed

--- SpeedyWheels/SpeedyWheels/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SpeedyWheels/SpeedyWheels/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -72,10 +72,10 @@ namespace SpeedyWheels.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            [Required]
-            [DataType(DataType.Text)]
-            [Display(Name = "User Name")]
-            public string UserName { get; set; }
+            //[Required]
+            //[DataType(DataType.Text)]
+            //[Display(Name = "User Name")]
+            //public string UserName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -121,7 +121,7 @@ namespace SpeedyWheels.Areas.Identity.Pages.Account
                 var user = CreateUser();
                 //user.UserName = Input.UserName; 
 
-                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
