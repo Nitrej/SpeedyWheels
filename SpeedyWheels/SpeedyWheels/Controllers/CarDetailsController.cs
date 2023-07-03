9db923d1dc2534cb6884c30016b7b9d15792e6c0
Nitrej <Patryk1Wroblewski@wp.pl>
2023-07-03 20:28:30 +0200

    Added renting action

--- SpeedyWheels/SpeedyWheels/Controllers/CarDetailsController.cs
+++ SpeedyWheels/SpeedyWheels/Controllers/CarDetailsController.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SpeedyWheels.Models;
 using SpeedyWheels.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Data;
+using Microsoft.Build.Framework;
 
 namespace SpeedyWheels.Controllers
 {
     public class CarDetailsController : Controller
     {
         private readonly RentalDataContext context;
-        public CarDetailsController(RentalDataContext context)
+        private readonly UserManager<User> userManager;
+        public CarDetailsController(RentalDataContext context, UserManager<User> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
         }
 
         public IActionResult Index(int id)
@@ -30,6 +37,33 @@ namespace SpeedyWheels.Controllers
                 SeatsCount = m.SeatsCount,
             });
             return View(car);
+        }
+        [HttpPost]
+        public IActionResult Rent(int id, string st, string ed)
+        {
+            DateTime start = DateTime.Parse(st);
+            DateTime end = DateTime.Parse(ed);
+            var rent = new Rental();
+
+            rent.RentDate = start.ToUniversalTime();
+            rent.CarId = id;
+
+            rent.ClientId = this.context.Clients.FirstOrDefault(i => i.UserId == this.User.FindFirstValue(ClaimTypes.NameIdentifier)).Id;
+
+            TimeSpan duration = end.Subtract(start);
+
+            if(duration.Minutes>0) rent.HourCount = duration.Hours + 1 + duration.Days * 24;
+            else rent.HourCount = duration.Hours + duration.Days * 24;
+
+            rent.Cost = rent.HourCount * this.context.Cars.FirstOrDefault(i => i.Id == id).CostPerHour;
+
+            this.context.Rentals.Add(rent);
+
+            this.context.Cars.FirstOrDefault(i => i.Id == id).IsRented = true;
+
+            this.context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
